d324ded4e05b89ad60f2c781649366b2e712fa59
amey <amey@LAPTOP-GHF5MGC8>
2022-04-20 14:50:27 +0530

    firs commit

---
+++ Day4_Packages/src/com/cg/Encapsulation/Encapsulation.java
@@ -0,0 +1,15 @@
+package com.cg.Encapsulation;
+
+import com.cg.p3.Sbi;
+
+public class Encapsulation {
+	public static void main(String[] args) 
+	{
+		Sbi obj=new Sbi();
+		obj.setbalance(57000);
+		System.out.println(obj.getBalance());
+		
+
+	}
+
+}
---
+++ Day4_Packages/src/com/cg/p3/Sbi.java
@@ -0,0 +1,15 @@
+package com.cg.p3;
+
+
+public class Sbi
+{
+	private int balance;
+	public int getBalance()
+	{
+		return balance;
+	}
+	public void setbalance (int bal)
+	{
+		balance=bal;
+	}
+}
