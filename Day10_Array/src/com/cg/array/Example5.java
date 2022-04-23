8714efb0633b3d968d25b46066be08797860e3d9
amey <amey@LAPTOP-GHF5MGC8>
2022-04-23 12:27:28 +0530

    first commit

---
+++ Day10_Array/src/com/cg/array/Example5.java
@@ -0,0 +1,10 @@
+package com.cg.array;
+
+public class Example5 {
+	public static void main(String[] args) {
+		int arr[]= {11,22,33,44,55};
+		System.out.println(arr.getClass().getName());
+
+	}
+
+}
