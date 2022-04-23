9306fbf6e0370f606b838b439ecfa22e1d2b98a2
amey <amey@LAPTOP-GHF5MGC8>
2022-04-23 12:30:37 +0530

    first commit

---
+++ Day12_Multithreading1.1/src/com/cg/multithreading/Example1.java
@@ -0,0 +1,18 @@
+package com.cg.multithreading;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		Thread t=Thread.currentThread();
+		System.out.println("Current thread: "+t);
+		try
+		{
+			Thread.sleep(1);
+		}
+		catch(InterruptedException e)
+		{
+			System.out.println(e);
+		}
+
+	}
+}
---
+++ Day12_Multithreading1.1/src/com/cg/multithreading/Example2.java
@@ -0,0 +1,21 @@
+package com.cg.multithreading;
+
+class A extends Thread
+{
+	public void run()
+	{
+		System.out.println("Thread is running");
+	}
+}
+public class Example2  {
+
+	public static void main(String[] args)
+	{
+		A obj=new A();
+		obj.start();
+		obj.start();
+
+	}
+
+}
+
