49389b7ce29865e8f76fa38cfd3c9b72e13852ee
amey <amey@LAPTOP-GHF5MGC8>
2022-04-23 12:26:22 +0530

    first commit

---
+++ Day9_Interface_Wrapper classes/src/com/cg/interface1/B.java
@@ -0,0 +1,21 @@
+package com.cg.interface1;
+
+ interface A {
+
+	public void print();
+
+
+class B implements A
+{
+	public void print()
+	{
+		System.out.println("Hey Guys,Welcome to the concept of interfaces");
+	}
+	public static void main(String args[])
+	{
+		B obj=new B();
+		obj.print();
+	}
+
+}
+}
