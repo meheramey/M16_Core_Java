272129563b87abee34cab609678de7c2c6d11633
amey <amey@LAPTOP-GHF5MGC8>
2022-05-24 10:25:15 +0530

    First commit

---
+++ Day24_Lambda Expression/src/com/cg/lambdaexp/Example2.java
@@ -0,0 +1,23 @@
+
+package com.cg.lambdaexp;
+
+interface B
+{
+	String display(String str);
+}
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		//Lambda Expression with return statement
+		//Lambda Expression with Single Parameter
+		B obj=(String str)->
+		{
+			
+			return str;
+		};
+		System.out.println(obj.display("Lambda Expression with Single Parameter"));
+
+	}
+
+}
